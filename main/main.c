ca3572a331fd578dee956da6364e8a23b976f518
EduardoKPVeiga <epontesveiga@gmail.com>
2023-07-31 13:23:52 -0300

    Test

--- main/main.c
+++ main/main.c
@@ -2,10 +2,20 @@
  * Application entry point.
 */
 
+#include "esp_log.h"
 #include "nvs_flash.h"
 
+#include "sntp_time_sync.h"
 #include "wifi_app.h"
 #include "wifi_reset_button.h"
+
+static const char TAG[] = "main";
+
+void wifi_application_connected_events(void)
+{
+    ESP_LOGI(TAG, "WiFi Application Connected!");
+    sntp_time_sync_task_start();
+}
 
 void app_main(void)
 {
@@ -22,5 +32,8 @@ void app_main(void)
     wifi_app_start();
 
     // Configure the Wifi reset button
-    wifi_reset_button_config(); 
+    wifi_reset_button_config();
+
+    // Set connected event callback
+    wifi_app_set_callback(&wifi_application_connected_events);
 }
--- main/wifi_app.h
+++ main/wifi_app.h
@@ -5,6 +5,9 @@
 #include "esp_wifi_types.h"
 #include "freertos/FreeRTOS.h"
 
+// Callback typedef
+typedef void (*wifi_connected_event_callback_t)(void);
+
 // WiFi application settings
 #define WIFI_AP_SSID                "ESP32_AP"      // AP name
 #define WIFI_AP_PASSWORD            "password"      // AP password
@@ -65,5 +68,15 @@ void wifi_app_start(void);
  * Gets the wifi configuration
 */
 wifi_config_t* wifi_app_get_wifi_config(void);
+
+/**
+ * Sets the callback function.
+*/
+void wifi_app_set_callback(wifi_connected_event_callback_t cb);
+
+/**
+ * Calls the callback function.
+*/
+void wifi_app_call_callback(void);
 
 #endif /* MAIN_WIFI_APP_H_ */
